0b04dc684675925690b6ab7b9d6248aad6bf91a6
Louisa Barrett <louisabarrett@gmail.com>
2013-12-18 17:28:43 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ColoradoBeasties::Application.config.secret_key_base = '2e2dec395c8d2e0911a6e694216e4b3cadc40526d9ec9affdca79898c047e203490d49d30a71ac8b9e7ec7172006dbd7fdd1d7a36ab84b414c5f20ab7918f70e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ColoradoBeasties::Application.config.session_store :cookie_store, key: '_colorado_beasties_session'
