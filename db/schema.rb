c7dbc963e4257a038396b3c70e8d2fc69390c50e
Louisa Barrett <louisabarrett@gmail.com>
2013-12-19 12:38:24 -0700

    set up animal and category tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219005214) do
+ActiveRecord::Schema.define(version: 20131219034356) do
 
   create_table "animals", force: true do |t|
     t.string   "name"
